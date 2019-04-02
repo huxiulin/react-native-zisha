0ff8458957b9b46b6bc450d044b0604901871bd5
joneihyH <huxiulin@anji-plus.com>
2019-04-02 15:28:09 +0800

    添加插件

--- app/application/src/root.js
+++ app/application/src/root.js
@@ -2,8 +2,12 @@
  * 项目主入口文件
  */
 
+ 
  import React,{Component} from "react";
  import {StatusBar,View,Text} from "react-native";
+ import {Provider} from "react-redux";
+ import {PersistGate} from "redux-persist/es/integration/react";
+ import SplashScreen from "react-native-splash-screen";
 
  export default class App extends Component {
    render() {
